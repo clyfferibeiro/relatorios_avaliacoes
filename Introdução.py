465caf0bde661ca172172a7b7f1f9c40b78ac5de
clyfferibeiro <165920262+clyfferibeiro@users.noreply.github.com>
2024-10-01 07:14:31 -0300

    Update Introdução.py

--- Introdução.py
+++ Introdução.py
@@ -17,5 +17,6 @@ with st.container():
     st.write("Navegue no menu do lado esquerdo para as tarefas desejadas.")
     st.markdown('---')
     st.title('Passos:')
-    st.write('**1.** Crie o Mapa de Conteúdos utilizando a aba ["Gerar Mapa de Conteúdos"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Mapas_de_Conte%C3%BAdo);')
-    
+    st.write('**1.** Crie e Salve o Mapa de Conteúdos utilizando a aba ["Gerar Mapa de Conteúdos"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Mapas_de_Conte%C3%BAdo);')
+    st.write('**2.** Gere a Tabela de Pontuação com base no Mapa de Conteúdos salvo no item 1 utilizando a aba ["Gerar Tabela de Pontuação"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Tabela_de_Pontua%C3%A7%C3%B5es);')
+    st.write('**1.** Gere os relatórios à partir da Tabela de Pontuação salva anteriormente utilizando a aba ["Gerar Relatórios"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Relat%C3%B3rios);')
