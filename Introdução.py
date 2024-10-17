4fa846776790689b31f9c1d5b79bddcd194f2e42
clyfferibeiro <165920262+clyfferibeiro@users.noreply.github.com>
2024-10-17 13:52:06 -0300

    Add files via upload

--- Introdução.py
+++ Introdução.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import numpy as np
 
 
-st.set_page_config( page_title="Aplicativo para Geração de Relatório de Avaliações", page_icon='🏫')
+st.set_page_config(layout="wide", page_title="Aplicativo para Geração de Relatório de Avaliações", page_icon='🏫')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -25,3 +25,11 @@ with st.container():
     #st.write('**1.** Crie e Salve o Mapa de Conteúdos utilizando a aba ["Gerar Mapa de Conteúdos"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Mapas_de_Conte%C3%BAdo);')
     st.info('**2.** Gere e Salve a Tabela de Pontuações (com base no Mapa de Conteúdos salvo no item 1) utilizando a aba ["Gerar Tabela de Pontuações"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Tabela_de_Pontua%C3%A7%C3%B5es);')
     st.info('**3.** Gere os relatórios (à partir da Tabela de Pontuações salva anteriormente) utilizando a aba ["Gerar Relatórios"](https://relatoriosavaliacoes-6rrmrmsjn4dtuxbkt9qk8x.streamlit.app/Gerar_Relat%C3%B3rios);')
+
+with st.container():
+    st.markdown('---')
+    st.title('Tutorial:')
+    video_file = open("tutorial.mp4", "rb")
+    video_bytes = video_file.read()
+
+    st.video(video_bytes)
